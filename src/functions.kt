100a8ea4aadd69a2c5a4b9f144125bd00b269def
Kartik Singh Chauhan <lkchauhan.725@gmail.com>
2018-05-20 14:09:05 +0530

    learned about functions

---
+++ src/functions.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Area of rectangle 3 X 4 is : " + area(3,4))
+    println("Area of square of side 4 is  : " + area(4))
+}
+
+fun area(base:Int,height:Int = base):Int {
+    return (base * height)
+}
