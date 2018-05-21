ad3eed9a91b818df750a63d6531904d65d68bbce
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-05-21 22:10:31 +0530

    Played with Unit
    Signed-off-by: Kartik Chauhan <lkchauhan.725@gmail.com>

--- src/functions.kt
+++ src/functions.kt
@@ -1,8 +1,13 @@
 fun main(args: Array<String>) {
     println("Area of rectangle 3 X 4 is : " + area(3,4))
     println("Area of square of side 4 is  : " + area(4))
+    sayHello()
 }
 
 fun area(base:Int,height:Int = base):Int {
     return (base * height)
+}
+
+fun sayHello(): Unit {
+    println("Hello")
 }
